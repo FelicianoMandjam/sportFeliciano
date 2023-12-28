36c5da818fd4d22534d2e2cf8b96dd5e028e430f
feliciano100jui <felicianomandjauber@gmail.com>
2023-12-28 12:50:13 +0100

    Sprint2-Ticket1-Feature:Système d'inscription avec validation de compte par email mis en place.

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,18 @@
 
 namespace App;
 
+use phpDocumentor\Reflection\Types\Parent_;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function __construct(string $environment, bool $debug)
+    {
+        \date_default_timezone_set("Europe/Paris");
+
+        parent::__construct($environment , $debug);
+    }
 }
