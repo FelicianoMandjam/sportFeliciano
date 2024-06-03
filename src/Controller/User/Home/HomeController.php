ae736c11429e8dd5d100ce159adba9c4cab7231d
Feliciano  MANDJAM <felicianomandja@gmail.com>
2024-06-03 11:35:41 +0200

    Maj

--- src/Controller/User/Home/HomeController.php
+++ src/Controller/User/Home/HomeController.php
@@ -2,9 +2,6 @@
 namespace App\Controller\User\Home;
 
 
-
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
